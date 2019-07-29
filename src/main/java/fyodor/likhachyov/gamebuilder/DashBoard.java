fad1a7b6da30788ab82ccc6dba05c7ae754b7530
notn0name <lixachev.fedor@gmail.com>
2019-07-29 17:38:10 +0400

    init comm

---
+++ src/main/java/fyodor/likhachyov/gamebuilder/DashBoard.java
@@ -0,0 +1,12 @@
+package fyodor.likhachyov.gamebuilder;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class DashBoard extends LinearLayout {
+
+    public DashBoard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ src/main/java/fyodor/likhachyov/gamebuilder/Field.java
@@ -0,0 +1,12 @@
+package fyodor.likhachyov.gamebuilder;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Field extends View {
+
+    public Field(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
